eea61ec7f1eaf5262c1cc656e77ed9e79dffbbc0
umang <umangmalviya51@gmail.com>
2023-10-24 19:20:38 +0530

    thirdcommit

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../components/Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+      <div className='navbar'>
+    <div>
+        <img src='https://web-images.credcdn.in/_next/assets/images/home-page/cred-logo.png' alt="logoimagenotfound"></img>
+    </div>
+    <ul> credit score check</ul>
+    <ul> Cred Pay</ul>
+      </div>
+    </div>
+  )
+}
+
+export {Navbar}
